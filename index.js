19f96308d3f3415521869fa3a7a909e077ad2885
Ekwebenem Chukwuebuka <collinschudy@gmail.com>
2025-03-01 01:51:30 +0100

    added the index.js page to add functionality to the input elements

---
+++ index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const calculateBtn = document.querySelector("button");
+  const amountElem = document.querySelector('input[name="mortgageAmount"]');
+  const termElem = document.querySelector('input[name="mortgageTerm"]');
+  const rateElem = document.querySelector('input[name="interestRate"]');
+  const resultBox = document.querySelector(".box1 h3");
+  const resultText = document.querySelector(".box1 p");
+
+  calculateBtn.addEventListener("click", function () {
+    let principal = amountElem.value;
+    let years = termElem.value;
+    let annualRate = rateElem.value;
+
+    let mortgageType = document.querySelector(
+      'input[name="mort_type"]:checked'
+    ).id;
+    // console.log(mortgageType);
+  
+    if (!principal.length || !years.length || !annualRate.length) {
+      alert("Please enter valid numbers");
+      return;
+    }
+
+    let r = annualRate / 100 / 12;
+    let n = years * 12;
+
+    let M, totalRepayment;
+
+    if (mortgageType === "repayment") {
+      M = (principal * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1); // google the formular to calculate mortgage
+      totalRepayment = M * n;
+    } else {
+      M = principal * r;
+      totalRepayment = M * n;
+    }
+
+    resultBox.textContent = `Monthly Payment: $${M.toFixed(2)}`;
+    resultText.textContent = `Total Repayment: $${totalRepayment.toFixed(2)}`;
+  });
+});
